36f8742b822b29733ffdedbd4b66f818cc46059e
Tim Vervelde <t.vervelde@gmail.com>
2014-10-13 13:08:34 +0200

    Added view folders and renamed form1.cs to FormMain.cs

--- Ict4Events/Reservation/Program.cs
+++ Ict4Events/Reservation/Program.cs
@@ -16,7 +16,7 @@ namespace Reservation
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormMain());
         }
     }
 }
