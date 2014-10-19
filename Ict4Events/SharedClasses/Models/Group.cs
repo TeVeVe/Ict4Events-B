7bd0a83f003f6fcd72a08a06efe532723528261b
Anton van Dijk <anton.van.dijk@live.nl>
2014-10-19 13:38:56 +0200

    Added 6 classes from diagram
    Group
    Privilege
    Product
    Rental
    UserAccount
    Wristband

---
+++ Ict4Events/SharedClasses/Models/Group.cs
@@ -0,0 +1,25 @@
+﻿
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SharedClasses.Models
+{
+    public class Group
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<Privilege> Privileges { get; set; }
+
+        public void Add(Privilege privilege)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AssignTo(UserAccount userAccount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Ict4Events/SharedClasses/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace SharedClasses.Models
+{
+    public class Product
+    {
+        public string Description { get; set; }
+        public Image Image { get; set; }
+        public bool Inactive { get; set; }
+        public string Name { get; set; }
+
+    }
+}
