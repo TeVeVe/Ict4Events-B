48f4725ac72a0a63c09f2553d1cf1dad43b94f0f
Anton van Dijk <anton.van.dijk@live.nl>
2014-10-19 13:51:26 +0200

    Added 4 classes from diagram
    Reservation
    Reservee
    Spot
    SpotType

---
+++ Ict4Events/SharedClasses/Models/Reservation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SharedClasses.Models
+{
+    public class Reservation
+    {
+        public bool PaymentStatus { get; set; }
+        public IEnumerable<Wristband> Wristbands { get; set; }
+        public Reservee Reservee { get; set; }
+
+        public Reservation(Reservee reservee, int amount)
+        {
+            Reservee = reservee;
+
+            // TODO: Implement wristband creation based on param "amount".
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Ict4Events/SharedClasses/Models/Reservee.cs
@@ -0,0 +1,30 @@
+﻿namespace SharedClasses.Models
+{
+    public class Reservee
+    {
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+        public string Insertion { get; set; }
+        public string LastName { get; set; }
+
+        public string HouseNumber { get; set; }
+        
+        public string PostalCode { get; set; }
+        public string Street { get; set; }
+
+        public string FullName
+        {
+            get { return FirstName + Insertion + LastName; }
+        }
+
+        public Reservee(string firstName, string insertion, string lastName, string postalCode, string street, string houseNumber)
+        {
+            FirstName = firstName;
+            Insertion = insertion;
+            LastName = lastName;
+            PostalCode = postalCode;
+            Street = street;
+            HouseNumber = houseNumber;
+        }
+    }
+}
