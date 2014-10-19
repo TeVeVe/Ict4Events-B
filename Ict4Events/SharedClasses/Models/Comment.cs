481f5764e66f9041a705092c5a0520b77e33e7c1
Tim Vervelde <t.vervelde@gmail.com>
2014-10-19 14:43:39 +0200

    Added the Comment and FeedPost classes.

---
+++ Ict4Events/SharedClasses/Models/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedClasses.Models
+{
+    class Comment
+    {
+        public string Content { get; set; }
+        public DateTime PostTime { get; set; }
+
+        public Comment(string content, UserAccount account)
+        {
+            Content = content;
+
+            throw new NotImplementedException();
+        }
+    }
+}
