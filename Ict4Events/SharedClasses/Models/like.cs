d4bee1d5cb6d656a07f8b1af1d7662f9206a8fea
Tim Vervelde <t.vervelde@gmail.com>
2014-10-19 14:34:23 +0200

    Added Location and Like classes plus the LikeType enum.

---
+++ Ict4Events/SharedClasses/Models/like.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedClasses.Models
+{
+    class Like
+    {
+        public LikeType Type { get; set; }
+    }
+}
