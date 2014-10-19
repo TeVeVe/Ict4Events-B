6bc2546f84778d34aa6a60a8b1f8f20a05cf6ea8
Tim Vervelde <t.vervelde@gmail.com>
2014-10-19 14:23:06 +0200

    Added the Event class

---
+++ Ict4Events/SharedClasses/Models/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedClasses.Models
+{
+    class Event
+    {
+        public DateTime EndDate { get; set; }
+        public int MaxGuests { get; set; }
+        public String Name { get; set; }
+        public DateTime StartDate { get; set; }
+
+        public int GetVisitorsInside()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
