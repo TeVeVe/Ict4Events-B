babd8444acad2fbc6fbca601add96a9d1ca83047
TeVeVe <t.vervelde@gmail.com>
2014-10-29 11:59:57 +0100

    Category en Comment models verbeterd.

--- Ict4Events/SharedClasses/Data/Models/Category.cs
+++ Ict4Events/SharedClasses/Data/Models/Category.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using SharedClasses.Data.Attributes;
 
 namespace SharedClasses.Data.Models
 {
+    [Table("CATEGORY")]
     public class Category
     {
+        [Key]
+        public int CategoryId { get; set; }
         public string Name { get; set; }
+        [FieldName("PARENTCATID")]
         public Category ParentCategory { get; set; }
+        public string Description { get; set; } 
 
         public void AddCategory(Category category)
         {
--- Ict4Events/SharedClasses/Data/Models/Comment.cs
+++ Ict4Events/SharedClasses/Data/Models/Comment.cs
@@ -1,13 +1,21 @@
 ﻿using System;
+using SharedClasses.Data.Attributes;
 
 namespace SharedClasses.Data.Models
 {
+    [Table("COMMENT")]
     public class Comment
     {
+        [Key]
+        public int CommentId { get; set; }
         public string Content { get; set; }
         public DateTime PostTime { get; set; }
+        [ForeignKey("PARENTID")]
         public Comment ParentComment { get; set; }
+        [ForeignKey("USERACCOUNTID")]
         public UserAccount UserAccount { get; set; }
+        [ForeignKey("FILEID")]
+        public File FileId { get; set; }
 
         public Comment(string content, UserAccount account)
         {
