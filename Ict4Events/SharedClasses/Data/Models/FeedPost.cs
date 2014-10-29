8f0d0753771a5577a8267ee5c262e2e3a855a3fd
TeVeVe <t.vervelde@gmail.com>
2014-10-29 12:55:22 +0100

    Improved Feedpost.cs and File.cs .

--- Ict4Events/SharedClasses/Data/Models/FeedPost.cs
+++ Ict4Events/SharedClasses/Data/Models/FeedPost.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using SharedClasses.Data.Attributes;
+using System;
 
 namespace SharedClasses.Data.Models
 {
+    [Table("FEEDPOST")]
     class FeedPost
     {
+        [Key]
+        public int FeedPostId { get; set; }
         public string Content { get; set; }
         public DateTime PostTime { get; set; }
+        [ForeignKey("USERACCOUNTID")]
         public UserAccount UserAccount { get; set; }
 
         public FeedPost(UserAccount account, string content)
--- Ict4Events/SharedClasses/Data/Models/File.cs
+++ Ict4Events/SharedClasses/Data/Models/File.cs
@@ -1,14 +1,20 @@
 ﻿using System;
+using SharedClasses.Data.Attributes;
 
 namespace SharedClasses.Data.Models
 {
+    [Table("FILE")]
     public class File
     {
+        [Key]
+        public int FileId { get; set; }
         public string Decription { get; set; }
         public string Name { get; set; }
         public DateTime PostTime { get; set; }
         public int ReportCount { get; set; }
+        [DbIgnore]
         public UserAccount UserAccount { get; set; }
+        [DbIgnore]
         public Category Category { get; set; }
 
         public File(UserAccount userAccount, string decription)
