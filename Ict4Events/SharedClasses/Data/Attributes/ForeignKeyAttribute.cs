3398a2cb0bc26438300bef20135abe7910ec6730
TeVeVe <t.vervelde@gmail.com>
2014-10-29 12:02:56 +0100

    Added the foreign key attribute

---
+++ Ict4Events/SharedClasses/Data/Attributes/ForeignKeyAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SharedClasses.Data.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ForeignKeyAttribute : System.Attribute
+    {
+        public Type TargetType { get; set; }
+        public string PropertyName { get; set; }
+
+        public ForeignKeyAttribute(string propertyName)
+        {
+            PropertyName = propertyName; 
+        }
+    }
+}
