108798733406bf344d2e45d07ed7359f07dc151f
Anton van Dijk <anton.van.dijk@live.nl>
2014-11-03 13:50:24 +0100

    New dll fix for database

--- Ict4Events/SharedClasses/Data/DataModel.cs
+++ Ict4Events/SharedClasses/Data/DataModel.cs
@@ -9,7 +9,7 @@ using System.Linq.Expressions;
 using System.Net;
 using System.Reflection;
 using System.Text;
-using Oracle.ManagedDataAccess.Client;
+using Oracle.DataAccess.Client;
 using SharedClasses.Data.Attributes;
 using SharedClasses.Extensions;
 
--- Ict4Events/SharedClasses/Data/Database.cs
+++ Ict4Events/SharedClasses/Data/Database.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using Oracle.ManagedDataAccess.Client;
+using Oracle.DataAccess.Client;
 using SharedClasses.Properties;
 
 namespace SharedClasses.Data
