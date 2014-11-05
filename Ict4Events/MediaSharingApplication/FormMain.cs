eef3db3318bafff1e62ad8997e84340dd8abd76b
teveve <t.vervelde@gmail.com>
2014-11-05 14:25:17 +0100

    Merged FormMain.cs and ControllerMVC.cs

--- Ict4Events/MediaSharingApplication/FormMain.cs
+++ Ict4Events/MediaSharingApplication/FormMain.cs
@@ -1,22 +1,14 @@
-﻿using System;using System.IO;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Net;
-using System.Text;
 using System.Windows.Forms;
-using MediaSharingApplication.Controllers;
-using SharedClasses;using MediaSharingApplication.Views;using SharedClasses.Data;
+using SharedClasses;
 using SharedClasses.Data.Models;
 using SharedClasses.MVC;
-using System.Linq;
 
 namespace MediaSharingApplication
 {
     public partial class FormMain : FormMVC
     {
-        public UserAccount UserSession { get; set; }
-
         public FormMain()
         {
             InitializeComponent();
@@ -34,9 +26,12 @@ namespace MediaSharingApplication
                     Debug.WriteLine(ofd.FileName);
                     filePath = ofd.FileName;
 
-                    categories.AddRange(new List<string> { "Media", "Muziek" });
+                    categories.AddRange(new List<string> {"Media", "Muziek"});
                     FileTransfer.UploadFile(filePath, categories);
                 }
-            };        }
+            };
+        }
+
+        public UserAccount UserSession { get; set; }
     }
 }
