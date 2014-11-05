b90bc51292a9ba8038b89472365c00af3dc0b9e2
teveve <t.vervelde@gmail.com>
2014-11-05 16:57:42 +0100

    Added FormMain.cs to the solution.

--- Ict4Events/MediaSharingApplication/FormMain.cs
+++ Ict4Events/MediaSharingApplication/FormMain.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.Windows.Forms;
+using MediaSharingApplication.Controllers;
 using SharedClasses;
 using SharedClasses.Data.Models;
 using SharedClasses.MVC;
@@ -13,23 +14,7 @@ namespace MediaSharingApplication
         {
             InitializeComponent();
 
-
-            Shown += (sender, args) =>
-            {
-                var categories = new List<string>();
-                string filePath = "";
-
-                var ofd = new OpenFileDialog();
-                DialogResult result = ofd.ShowDialog();
-                if (result == DialogResult.OK)
-                {
-                    Debug.WriteLine(ofd.FileName);
-                    filePath = ofd.FileName;
-
-                    categories.AddRange(new List<string> {"Media", "Muziek"});
-                    FileTransfer.UploadFile(filePath, categories);
-                }
-            };
+            MarkAsMain<ControllerMain>();
         }
 
         public UserAccount UserSession { get; set; }
