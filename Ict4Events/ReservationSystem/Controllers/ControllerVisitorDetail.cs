d83de28d4a80949e2206283ac8adb47ebbd3216d
Daan Franken <dgmfranken@gmail.com>
2014-11-06 13:53:16 +0100

    conformation email feature complete

--- Ict4Events/ReservationSystem/Controllers/ControllerVisitorDetail.cs
+++ Ict4Events/ReservationSystem/Controllers/ControllerVisitorDetail.cs
@@ -1,15 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using ReservationSystem.Views;
 using SharedClasses.MVC;
 
 namespace ReservationSystem.Controllers
 {
-    class ControllerVisitorDetail : ControllerMVC<ViewVisitorsDetail>
+    internal class ControllerVisitorDetail : ControllerMVC<ViewVisitorsDetail>
     {
+        private string emailTo;
+        private string name;
+
         public ControllerVisitorDetail()
         {
             View.ButtonCancelClick += ViewOnButtonCancelClick;
@@ -24,6 +26,52 @@ namespace ReservationSystem.Controllers
         private void ViewOnButtonSaveClick(object sender, EventArgs eventArgs)
         {
             MainForm.ActiveMenuItemText = "Bezoekers";
+            // TODO:  Validation of data
+            // TODO: Insert data in database
+
+            emailTo = View.textBoxEmail.Text;
+            name = View.textBoxName.Text;
+            SendReservationConfirmationEmail(emailTo);
+        }
+
+        public void SendReservationConfirmationEmail(string email)
+        {
+            string smtpAddress = "smtp.gmail.com";
+            int portNumber = 587;
+            bool enableSSL = true;
+
+            string emailFrom = "reservationict4events@gmail.com";
+            string password = "Administrator01";
+
+            string subject = string.Format("Ict4Events: Uw reservering is succesvol verwerkt!");
+            string body =
+                string.Format(
+                    "Geachte {0}, <br><br>bij deze is uw reservering succesvol verwerkt in ons systeem! <br>Uw bestelde polsbandjes zullen zo snel mogelijk naar u verstuurd worden. <br>" +
+                    "Op de dag van het evenement kunt u dit polsbandje gebruiken om toegang te krijgen tot het evenemententerein. <br>" +
+                    "U kunt het te betalen bedrag overmaken naar het volgende rekeningnummer: <b>123456</b>.<br><br>" +
+                    "Veel plezier op het event!", name);
+
+            using (var mail = new MailMessage())
+            {
+                mail.From = new MailAddress(emailFrom);
+                mail.To.Add(emailTo);
+                mail.Subject = subject;
+                mail.Body = body;
+                mail.IsBodyHtml = true;
+
+
+                using (var smtp = new SmtpClient(smtpAddress, portNumber))
+                {
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.UseDefaultCredentials = false;
+                    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtp.Credentials = new NetworkCredential(emailFrom, password);
+                    smtp.EnableSsl = enableSSL;
+                    smtp.Timeout = 10000;
+                    smtp.Send(mail);
+                }
+            }
         }
     }
 }
