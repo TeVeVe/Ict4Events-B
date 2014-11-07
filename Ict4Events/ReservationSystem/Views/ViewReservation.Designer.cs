821d263f7e0d19dfa6aef53331e30f2b5f5f25b3
Anton van Dijk <anton.van.dijk@live.nl>
2014-11-07 23:50:31 +0100

    Merge fix

--- Ict4Events/ReservationSystem/Views/ViewReservation.Designer.cs
+++ Ict4Events/ReservationSystem/Views/ViewReservation.Designer.cs
@@ -39,72 +39,15 @@
             // dataGridViewVisitors
             // 
             this.dataGridViewVisitors.Dock = System.Windows.Forms.DockStyle.Fill;
-<<<<<<< HEAD
             this.dataGridViewVisitors.Location = new System.Drawing.Point(0, 31);
-=======
             this.dataGridViewVisitors.Location = new System.Drawing.Point(0, 134);
->>>>>>> origin/ReserveringsSysteem
             this.dataGridViewVisitors.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridViewVisitors.Name = "dataGridViewVisitors";
             this.dataGridViewVisitors.RowTemplate.Height = 24;
             this.dataGridViewVisitors.Size = new System.Drawing.Size(497, 446);
             this.dataGridViewVisitors.TabIndex = 20;
-<<<<<<< HEAD
             this.dataGridViewVisitors.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewVisitors_CellDoubleClick);
-=======
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(201, 22);
-            this.button2.Margin = new System.Windows.Forms.Padding(2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(22, 21);
-            this.button2.TabIndex = 27;
-            this.button2.Text = "...";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(148, 66);
-            this.button5.Margin = new System.Windows.Forms.Padding(2);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 24);
-            this.button5.TabIndex = 26;
-            this.button5.Text = "Leegmaken";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(68, 66);
-            this.button4.Margin = new System.Windows.Forms.Padding(2);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 24);
-            this.button4.TabIndex = 25;
-            this.button4.Text = "Actualiseren";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(4, 48);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(61, 13);
-            this.label3.TabIndex = 23;
-            this.label3.Text = "Startdatum:";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(4, 26);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 13);
-            this.label2.TabIndex = 21;
-            this.label2.Text = "Evenement:";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
->>>>>>> origin/ReserveringsSysteem
+            
             // 
             // button7
             // 
@@ -121,11 +64,8 @@
             this.panel2.Controls.Add(this.button7);
             this.panel2.Controls.Add(this.buttonAddReservation);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-<<<<<<< HEAD
             this.panel2.Location = new System.Drawing.Point(0, 0);
-=======
             this.panel2.Location = new System.Drawing.Point(0, 103);
->>>>>>> origin/ReserveringsSysteem
             this.panel2.Margin = new System.Windows.Forms.Padding(2);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(497, 31);
@@ -141,84 +81,14 @@
             this.buttonAddReservation.Text = "Toevoegen";
             this.buttonAddReservation.UseVisualStyleBackColor = true;
             this.buttonAddReservation.Click += new System.EventHandler(this.buttonAddReservation_Click);
+            
             // 
-<<<<<<< HEAD
-=======
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(69, 24);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(128, 20);
-            this.textBox2.TabIndex = 20;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(41, 4);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(24, 13);
-            this.label1.TabIndex = 17;
-            this.label1.Text = "Lid:";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(69, 2);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(128, 20);
-            this.textBox1.TabIndex = 16;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(201, 0);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(22, 21);
-            this.button1.TabIndex = 19;
-            this.button1.Text = "...";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.dateTimePicker1);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.button5);
-            this.panel1.Controls.Add(this.button4);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.textBox2);
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(497, 103);
-            this.panel1.TabIndex = 18;
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(69, 44);
-            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(155, 20);
-            this.dateTimePicker1.TabIndex = 28;
-            // 
->>>>>>> origin/ReserveringsSysteem
             // ViewReservation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.dataGridViewVisitors);
             this.Controls.Add(this.panel2);
-<<<<<<< HEAD
-=======
-            this.Controls.Add(this.panel1);
->>>>>>> origin/ReserveringsSysteem
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "ViewReservation";
             this.Size = new System.Drawing.Size(497, 477);
@@ -230,26 +100,9 @@
 
         #endregion
 
-<<<<<<< HEAD
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button buttonAddReservation;
-=======
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Button buttonAddReservation;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
->>>>>>> origin/ReserveringsSysteem
         public System.Windows.Forms.DataGridView dataGridViewVisitors;
     }
 }
