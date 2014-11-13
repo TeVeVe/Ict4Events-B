1c05ba5e9e237596275ab00873379fc2a5d01bbd
teveve <t.vervelde@gmail.com>
2014-11-13 18:14:59 +0100

    Added images to the files, stopped using the old PanelTiles.

--- Ict4Events/SharedClasses/Controls/WinForms/PanelTile.cs
+++ Ict4Events/SharedClasses/Controls/WinForms/PanelTile.cs
@@ -1,51 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SharedClasses.Controls.WinForms
 {
-    public class PanelTile : Panel
+    public partial class PanelTile : UserControl
     {
-        public PanelTile(string primaryText, string secondaryText, string imagePath = null)
+        public PanelTile(string primaryText, string secondaryText, string image = null)
         {
-            BackColor = Color.Red;
+            InitializeComponent();
 
-            Width = 140;
-            Height = 140;
-
-            Panel textPanel = new Panel();
-            textPanel.BackColor = Color.Black;
-            Controls.Add(textPanel);
-            textPanel.Height = 30;
-            textPanel.Dock = DockStyle.Bottom;
-
-            Label primaryLabel = new Label();
-            textPanel.Controls.Add(primaryLabel);
-            primaryLabel.Text = primaryText;
-            primaryLabel.ForeColor = Color.White;
-            primaryLabel.AutoSize = true;
-            
-            Label secondaryLabel = new Label();
-            textPanel.Controls.Add(secondaryLabel);
-            secondaryLabel.Text = secondaryText;
-            secondaryLabel.ForeColor = Color.White;
-            secondaryLabel.Location = new Point(0, 15);
-            secondaryLabel.AutoSize = true;
-            secondaryLabel.Font = new Font(secondaryLabel.Font.FontFamily, 6);
-        }
-
-        protected override void OnMouseHover(EventArgs e)
-        {
-            BorderStyle = BorderStyle.FixedSingle;
-            base.OnMouseHover(e);
-        }
-
-
-
-        protected override void OnMouseLeave(EventArgs e)
-        {
-            BorderStyle = BorderStyle.None;
-            base.OnMouseLeave(e);
+            lblFileName.Text = primaryText;
+            lblDescription.Text = secondaryText;
         }
     }
 }
