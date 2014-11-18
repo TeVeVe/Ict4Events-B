3ff00d12e09a80a91f3ec00e9e836aa4395fe27e
Wondergem <cainite@live.nl>
2014-11-18 16:42:52 +0100

    new view

--- Ict4Events/SharedClasses/Data/Models/Rental.cs
+++ Ict4Events/SharedClasses/Data/Models/Rental.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Linq;
 using SharedClasses.Data.Attributes;
+using SharedClasses.Extensions;
 
 namespace SharedClasses.Data.Models
 {
@@ -10,7 +12,8 @@ namespace SharedClasses.Data.Models
         [Key]
         [FieldName("RENTALID")]
         public int Id { get; set; }
-
+        [DbIgnore]
+        [Browsable(false)]
         public string VisitorCode { get; set; }
         [DbIgnore]
         [Browsable(false)]
@@ -22,6 +25,18 @@ namespace SharedClasses.Data.Models
         [System.ComponentModel.DisplayName("Ingangsdatum")]
         public DateTime StartTime { get; set; }
 
+
         public int ProductId { get; set; }
+
+        [DisplayName("Productnaam")]
+        [DbIgnore]
+        public string Productname
+        {
+            get
+            {
+                var product = Product.Select("PRODUCTID = " + ProductId.ToSqlFormat()).FirstOrDefault();
+                return product.Name;
+            }
+        }
     }
 }
