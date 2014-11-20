0528e4ae3042716e1873a06e9ba5ef9e1b653eb0
Daan Franken <dgmfranken@gmail.com>
2014-11-20 12:26:54 +0100

    Added option to delete rentals (when "turned in")

--- Ict4Events/ReservationSystem/Views/ViewVisitorDetail.cs
+++ Ict4Events/ReservationSystem/Views/ViewVisitorDetail.cs
@@ -7,14 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SharedClasses.Data.Models;
 
 namespace ReservationSystem.Views
 {
     public partial class ViewVisitorDetail : UserControl
     {
+        public event EventHandler ButtonDeleteRentalClick;
+
+        protected virtual void OnButtonDeleteRentalClick()
+        {
+            EventHandler handler = ButtonDeleteRentalClick;
+            if (handler != null)
+                handler(this, EventArgs.Empty);
+        }
+
         public ViewVisitorDetail()
         {
             InitializeComponent();
+        }
+
+        private void buttonDeleteRental_Click(object sender, EventArgs e)
+        {
+            OnButtonDeleteRentalClick();
         }
     }
 }
