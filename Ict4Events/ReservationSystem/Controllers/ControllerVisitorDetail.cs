0ae2307503181840da2953ef368935cabdc88993
Daan Franken <dgmfranken@gmail.com>
2014-11-20 12:26:54 +0100

    Added option to delete rentals (when "turned in")

--- Ict4Events/ReservationSystem/Controllers/ControllerVisitorDetail.cs
+++ Ict4Events/ReservationSystem/Controllers/ControllerVisitorDetail.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using ReservationSystem.Views;
+using SharedClasses.Data.Models;
 using SharedClasses.MVC;
 
 namespace ReservationSystem.Controllers
@@ -12,7 +14,19 @@ namespace ReservationSystem.Controllers
     {
         public ControllerVisitorDetail()
         {
-            
+            View.ButtonDeleteRentalClick += OnButtonDeleteRentalClick;
+        }
+
+        public override void Activate()
+        {
+            IEnumerable<Rental> rentals = Rental.Select();//Rental.Select("VISITORCODE = " + reservee.VisitorCode.ToSqlFormat());
+            View.extendedDataGridView1.DataSource = rentals.ToList();
+        }
+
+        private void OnButtonDeleteRentalClick(object sender, EventArgs e)
+        {
+            var rental = (Rental)View.extendedDataGridView1.SelectedCells[0].OwningRow.DataBoundItem;
+            rental.Delete();
         }
     }
 }
--- Ict4Events/ReservationSystem/Views/ViewVisitorDetail.Designer.cs
+++ Ict4Events/ReservationSystem/Views/ViewVisitorDetail.Designer.cs
@@ -28,10 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.extendedDataGridView1 = new SharedClasses.Controls.WinForms.ExtendedDataGridView();
+            this.buttonDeleteRental = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.extendedDataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // extendedDataGridView1
+            // 
+            this.extendedDataGridView1.AllowUserToResizeRows = false;
+            this.extendedDataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.extendedDataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.extendedDataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.extendedDataGridView1.Location = new System.Drawing.Point(0, 32);
+            this.extendedDataGridView1.Name = "extendedDataGridView1";
+            this.extendedDataGridView1.ShowCellToolTips = false;
+            this.extendedDataGridView1.Size = new System.Drawing.Size(745, 398);
+            this.extendedDataGridView1.TabIndex = 0;
+            // 
+            // buttonDeleteRental
+            // 
+            this.buttonDeleteRental.Location = new System.Drawing.Point(17, 3);
+            this.buttonDeleteRental.Name = "buttonDeleteRental";
+            this.buttonDeleteRental.Size = new System.Drawing.Size(75, 23);
+            this.buttonDeleteRental.TabIndex = 1;
+            this.buttonDeleteRental.Text = "Verwijderen";
+            this.buttonDeleteRental.UseVisualStyleBackColor = true;
+            this.buttonDeleteRental.Click += new System.EventHandler(this.buttonDeleteRental_Click);
+            // 
+            // ViewVisitorDetail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.buttonDeleteRental);
+            this.Controls.Add(this.extendedDataGridView1);
+            this.Name = "ViewVisitorDetail";
+            this.Size = new System.Drawing.Size(745, 430);
+            ((System.ComponentModel.ISupportInitialize)(this.extendedDataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        public System.Windows.Forms.Button buttonDeleteRental;
+        public SharedClasses.Controls.WinForms.ExtendedDataGridView extendedDataGridView1;
     }
 }
